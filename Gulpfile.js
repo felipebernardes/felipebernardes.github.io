fad5f3e9a9a9dbf25fb745fdbaf7681a256d53a4
Felipe Bernardes <bernardes.flp@gmail.com>
2015-08-20 02:35:32 -0300

    Reviews gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+gulp.task('dependencies', function() {
+    gulp.src('bower_components/font-awesome/scss/font-awesome.scss')
+        .pipe(gulp.dest('./scss/libs/_font-awesome.scss'))
+//    .pipe(gulp.src('bower_components/normalize-scss/_normalize.scss')
+//        .pipe(gulp.dest('./scss/libs/')))
+});
+
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('scss/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'))
 });
 
 
 gulp.task('watch',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('scss/**/*.scss',['styles']);
 });
