013b0bdf3ee83192e5f6f7615837687624bf7191
bernardes_felipe <bernardes.flp@gmail.com>
2015-09-01 02:17:48 -0300

    Matches folder structure to github pages patterns

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,7 @@ var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
 gulp.task('dependencies', function() {
-    gulp.src('bower_components/components-font-awesome/scss/font-awesome.scss')
+    gulp.src('bower_components/components-font-awesome/css/font-awesome.min.css')
         .pipe(rename('_font-awesome.scss'))
         .pipe(gulp.dest('./scss/libs/'));
 
@@ -15,7 +15,7 @@ gulp.task('dependencies', function() {
 gulp.task('styles', function() {
     gulp.src('scss/style.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./www/css/'))
+        .pipe(gulp.dest('./css/'))
 });
 
 
@@ -26,8 +26,8 @@ gulp.task('watch',function() {
 gulp.task('sync', function() {
 
     browserSync.init({
-        server: "www/"
+        server: ""
     });
     gulp.watch("scss/*.scss", ['styles']);
-    gulp.watch("www/*.html").on('change', browserSync.reload);
+    gulp.watch("index.html").on('change', browserSync.reload);
 });
