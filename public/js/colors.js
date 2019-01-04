b1248cca27cbc924344acf4f88b699464160a728
Felipe Bernardes <bernardes.flp@gmail.com>
2019-01-04 02:24:00 -0200

    Compile sources

--- public/js/colors.js
+++ public/js/colors.js
@@ -4,27 +4,27 @@
   root.addEventListener('mousemove', (e) => {
     const viewportWidth = window.innerWidth;
     const cursorPosition = e.pageX;
-    const currentPercentage = Math.floor(cursorPosition / viewportWidth * 100);
+    const currentPositionPercentage = Math.floor(cursorPosition / viewportWidth * 100);
 
-    if (currentPercentage < 25) {
+    if (currentPositionPercentage < 25) {
       root.style.setProperty('--color-main-light', getComputedStyle(root).getPropertyValue('--pink-light'));
       root.style.setProperty('--color-main-dark', getComputedStyle(root).getPropertyValue('--pink-dark'));
     }
 
-    if (currentPercentage >= 25 && currentPercentage < 50) {
+    if (currentPositionPercentage >= 25 && currentPositionPercentage < 50) {
       root.style.setProperty('--color-main-light', getComputedStyle(root).getPropertyValue('--yellow-light'));
       root.style.setProperty('--color-main-dark', getComputedStyle(root).getPropertyValue('--yellow-dark'));
     }
 
-    if (currentPercentage >= 50 && currentPercentage < 75) {
+    if (currentPositionPercentage >= 50 && currentPositionPercentage < 75) {
       console.log('green');
       root.style.setProperty('--color-main-light', getComputedStyle(root).getPropertyValue('--green-light'));
       root.style.setProperty('--color-main-dark', getComputedStyle(root).getPropertyValue('--green-dark'));
     }
 
-    if (currentPercentage >= 75) {
+    if (currentPositionPercentage >= 75) {
       root.style.setProperty('--color-main-light', getComputedStyle(root).getPropertyValue('--blue-light'));
       root.style.setProperty('--color-main-dark', getComputedStyle(root).getPropertyValue('--blue-dark'));
     }
-  })
+  });
 })();
