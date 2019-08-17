bbac5fa75d95b7351df4b647d2b43b16c9129367
Felipe Bernardes <bernardes.flp@gmail.com>
2019-08-17 18:39:23 -0300

    Update PROD version

--- js/timeline.js
+++ js/timeline.js
@@ -5,18 +5,20 @@
   const titles = document.querySelectorAll('.entries__item__title');
   const details = document.querySelectorAll('.entries__item__detail');
 
-  let lastActiveIndex;
+  let lastActiveIndex = 0;
+
   const hidePreviouslyActive = () => {
-    $(entries[lastActiveIndex]).removeClass('active');
-    $(details[lastActiveIndex]).removeClass('active');
+    if (typeof lastActiveIndex === 'undefined') return;
+    entries[lastActiveIndex].classList.remove('active');
+    details[lastActiveIndex].classList.remove('active');
   };
 
   titles.forEach((t, i) => {
     if (i === lastActiveIndex) return;
     t.addEventListener('mouseover', (ev) => {
       hidePreviouslyActive();
-      $(entries[i]).addClass('active');
-      $(details[i]).addClass('active');
+      entries[i].classList.add('active');
+      details[i].classList.add('active');
       lastActiveIndex = i;
     });
   });
