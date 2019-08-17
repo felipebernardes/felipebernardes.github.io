403f1fa8b708bed06823a11ffaccb388dd19803e
Felipe Bernardes <bernardes.flp@gmail.com>
2019-08-17 05:02:44 -0300

    Review gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,54 +1,55 @@
 const gulp = require('gulp'),
-autoprefixer = require('gulp-autoprefixer'),
-sass = require('gulp-sass'),
-browserSync = require('browser-sync').create();
-responsive = require('gulp-responsive');
-imgMin = require('gulp-imagemin');
-
-gulp.task('dependencies', () => {
-  gulp.src('node_modules/normalize-scss/_normalize.scss')
-  .pipe(gulp.dest('./src/scss/libs/'));
-});
+      autoprefixer = require('gulp-autoprefixer'),
+      sass = require('gulp-sass'),
+      browserSync = require('browser-sync').create();
+      responsive = require('gulp-responsive');
+      imgMin = require('gulp-imagemin');
 
 gulp.task('js', () => {
   gulp.src('./src/js/*.js')
-  .pipe(gulp.dest('./public/js/'));
+  .pipe(gulp.dest('./dist/js/'));
 });
 
 gulp.task('scss', () => {
   gulp.src('./src/scss/style.scss')
   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
   .pipe(autoprefixer({browsers: ['last 10 versions'], cascade: false}))
-  .pipe(gulp.dest('./public/css/'));
+  .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('html', () => {
   gulp.src('./src/*.html')
-  .pipe(gulp.dest('./public/'));
+  .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('img', () => {
   gulp.src('./src/img/*.*')
   .pipe(imgMin())
-  .pipe(gulp.dest('./public/img/'));
+  .pipe(gulp.dest('./dist/img/'));
 
   gulp.src('./src/img/favicon/*.*')
   .pipe(imgMin())
-  .pipe(gulp.dest('./public/img/favicon/'));
+  .pipe(gulp.dest('./dist/img/favicon/'));
 
   gulp.src('./src/img/timeline/*.*')
   .pipe(responsive({ '*': { width: 370 } }))
   .pipe(imgMin())
-  .pipe(gulp.dest('./public/img/timeline/'));
+  .pipe(gulp.dest('./dist/img/timeline/'));
+});
+
+gulp.task('compile:dev', ['js', 'scss', 'html', 'img']);
+gulp.task('compile:prod', () => {
+  gulp.src(['./dist/*.*', './dist/**/*.*'])
+  .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', () => {
-  browserSync.init({server: "./public/"});
+  browserSync.init({server: "./dist/"});
 
   gulp.watch("./src/js/**/*.js", ['js']);
   gulp.watch("./src/scss/**/*.scss", ['scss']);
   gulp.watch("./src/*.html", ['html']);
   gulp.watch("./src/img/*.*", ['img']);
 
-  gulp.watch("./public/**/*.*").on('change', browserSync.reload);
+  gulp.watch("./dist/**/*.*").on('change', browserSync.reload);
 });
